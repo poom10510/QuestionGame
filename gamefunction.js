7aa8c4226016c49ee2ce3363e3e2f0d250a294a0
poom <poom10510@gmail.com>
2018-02-24 18:27:58 +0700

    add game system

--- gamefunction.js
+++ gamefunction.js
@@ -2,42 +2,65 @@
 // live-server
 
 var timestart = false;
-var timeleft = 10;
-
+var timemax = 10;
+var timeleft = timemax;
 var qcontainer = [];
 // Update the count down every 1 second
 var counttime = setInterval(function() {
     if (timeleft <= 0) {
         //clearInterval(x);
-        changeQuestion()
-            //document.getElementById("demo").innerHTML = "EXPIRED";
+        changeQuestion();
+        //document.getElementById("demo").innerHTML = "EXPIRED";
     }
     if (timestart) {
-        document.getElementById("timer").innerHTML = timeleft;
+        document.getElementById("timer").innerHTML = "Time: " + timeleft;
         timeleft--;
     }
 }, 1000);
 
 var qindex = -1;
-var score = 0;
+var scorenow = 0;
+var questionnow = 1;
+var questionlimit = 5;
+var answerrecord = [];
 
 function game(data) {
     console.log('game should be start with ', data)
     qcontainer = data;
-    qindex = changeQuestion();
+    changeQuestion();
+}
 
+function startGame() {
+    resetGame();
+    changeStatetime();
+}
 
+function resetGame() {
+    scorenow = 0;
+    questionnow = 1;
+    timeleft = timemax;
+    answerrecord = [];
+    updateScore();
+    document.getElementById("questionshow").textContent = "Question: " + questionnow;
 
 }
 
-function myFunction() {
+function summaryGame() {
+    $("#questionboard").hide();
+    $("#summaryboard").toggle();
+    console.log(answerrecord);
+
+}
+
+
+function changeStatetime() {
     console.log(1);
     timestart = !timestart;
 }
 
 function getquestion() {
     qindex = changeQuestion();
-    //score++;
+    //scorenow++;
     updateScore()
 }
 
@@ -51,13 +74,26 @@ function changeQuestion() {
 
     console.log(x + " " + q);
     setQuestion(qcontainer[x]);
-    timeleft = 10;
-    return x;
+    timeleft = timemax;
+    qindex = x;
 
 }
 
 function updateScore() {
-    document.getElementById("scorenow").textContent = "Score: " + score;
+    document.getElementById("scoreshow").textContent = "Score: " + scorenow;
+}
+
+function updateQuestion() {
+    console.log(questionnow);
+    if (questionlimit - questionnow > 0) {
+        questionnow++;
+        document.getElementById("questionshow").textContent = "Question: " + questionnow;
+    } else {
+        changeStatetime();
+        summaryGame();
+
+    }
+
 }
 
 function setQuestion(item) {
@@ -66,19 +102,27 @@ function setQuestion(item) {
     document.getElementById("c2").textContent = item.choice[1];
     document.getElementById("c3").textContent = item.choice[2];
     document.getElementById("c4").textContent = item.choice[3];
+}
+
+function setQuestionvalue(num) {
+    questionlimit = num;
+    document.getElementById("startselect").textContent = "Start " + questionlimit + " Questions";
 
 }
 
 function selectAnswer(num) {
     if (timestart) {
         var key = qcontainer[qindex].answer;
+        answerrecord.push({ "answer": num, "key": key, "time": 10 - timeleft });
         if (num == key) {
             console.log(num + " correct!");
-            score++;
+            scorenow++;
         } else {
             console.log(num + " incorrect!");
         }
-        qindex = changeQuestion();
-        updateScore()
+
+        changeQuestion();
+        updateScore();
+        updateQuestion();
     }
 }
